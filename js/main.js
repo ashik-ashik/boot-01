91bf6ee6923b8dee0463f0d5f2d47784b6b49e08
Ashik <kmdashik635@gmail.com>
2020-11-18 09:24:08 +0600

    files upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+  });
